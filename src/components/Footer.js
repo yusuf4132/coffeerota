ed621948436e6a85b15bdf3811fcf7e30f99225c
Yusuf <baysagdicyusuf@gmail.com>
2025-12-24 15:17:33 +0300

    iki

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
         <Logo />
         <div className="site-links">
           <nav className="links-container">
-          <Link style={{ paddingRight: "20px" }} to="/">Ana Sayfa</Link>
+            <Link style={{ paddingRight: "20px" }} to="/">Ana Sayfa</Link>
             <Link style={{ paddingRight: "20px" }} to="/about">Hakkımızda</Link>
             <Link style={{ paddingRight: "20px" }} to="/menu">Menü</Link>
             <Link to="/visit">Ziyaret Edin</Link>
@@ -23,14 +23,14 @@ const Footer = () => {
               <span className="icon fa-solid fa-location-dot fa-fw" aria-hidden="true"></span> <address>555 Espresso Lane Mochaville FL 55555</address>
             </div>
             <div className="info phone"><span className="icon fa-solid fa-phone fa-fw" aria-hidden="true"></span> <address>
-                <a href="tel:5415613845">(541) 561-3845</a>
+              <a href="tel:5415613845">(541) 561-3845</a>
             </address></div>
           </div>
           <div className="social-media-links">
             {/*<a href="" target="_blank" aria-label="Our Facebook page" title="Facebook">
               <span className="fa-brands fa-facebook fa-fw"></span>
   </a>*/}
-            <a href="https://www.instagram.com/coffeerota/" target="_blank" aria-label="coffeerota" title="Instagram">İnstagram Adresimiz İçin Tıklayın :
+            <a href="https://www.instagram.com/coffeerota/" target="_blank" aria-label="coffeerota" title="Instagram">İnstagram:
               <span className="fa-brands fa-instagram fa-fw"></span>
             </a>
             {/*<a href="" target="_blank" aria-label="Our Bluesky account" title="Bluesky">
@@ -40,8 +40,8 @@ const Footer = () => {
         </div>
       </div>
       <hr />
-      <nav style={{display:'flex',justifyContent:'flex-end',marginTop:'20px'}}>
-      <NavLink style={{ paddingRight: "20px"}} to="/admin-login">Yönetici Girişi</NavLink>
+      <nav style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}>
+        <NavLink style={{ paddingRight: "20px" }} to="/admin-login">Yönetici Girişi</NavLink>
       </nav>
       <div className="app-footer">Tüm Hakları Saklıdır {new Date().getFullYear()}</div>
     </footer>
